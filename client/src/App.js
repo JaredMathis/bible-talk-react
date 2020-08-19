1aea9c5221d82f6e776561c0a6ae31a1746efcd5
JaredMathis <64503422+JaredMathis@users.noreply.github.com>
2020-08-19 13:11:46 -0400

    update

--- client/src/App.js
+++ client/src/App.js
@@ -8,38 +8,73 @@ const url = 'http://bible-api.s3-website-us-east-1.amazonaws.com/';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { books: [] };
+    this.state = {};
   }
   componentDidMount = () => {
     axios.get(url + 'translation/kjv/books.json').then(response => {
       let books = response.data.map(b => { 
-        console.log({b})
         return {
           name: b.replace('-', ' '),
           id: b,
         }
       });
       this.setState({books});
-      this.setState({selectedBook:books[0].name});
+      this.updateBook(books[0].name);
     });
   }
   bookChanged = (e) => {
     let name = e.target.value;
+    this.updateBook(name);
+  }
+  updateBook = (name) => {
     this.setState({selectedBook:name});
+    axios.get(url + 'translation/kjv/book/' + name + '/chapters.json').then(response => {
+      const chapters = response.data;
+      this.setState({chapters});
+      this.updateChapter(chapters[0]);
+    });
+  }
+  chapterChanged = (e) => {
+    let name = e.target.value;
+    this.updateChapter(name);
+  }
+  updateChapter = (name) => {
+    this.setState({selectedChapter:name});
+
+    console.log('updateChapter');
+    axios.get(url + 'translation/kjv/book/' 
+      + this.state.selectedBook + '/chapter/' 
+      + this.state.selectedChapter + '/verses.json').then(response => {
+      this.setState({verses: response.data});
+    });
   }
   render() {
-    let list = this.state.books.map(b => 
+    let books = this.state.books 
+      && this.state.books.map(b => 
       <option key={b.id}>
         {b.name}
+      </option>);
+    let chapters = this.state.chapters 
+      && this.state.chapters.map(c => 
+      <option key={c}>
+        {c}
+      </option>);
+    let verses = this.state.verses 
+      && this.state.verses.map(v => 
+      <option key={v}>
+        {v}
       </option>);
     return (
       <div>
         <select onChange={this.bookChanged}>
-          {list}
+          {books}
         </select>
-        <span>
-          {this.state.selectedBook}
-        </span>
+        <select onChange={this.chapterChanged}>
+          {chapters}
+        </select>
+        <select onChange={this.verseChanged}>
+          {verses}
+        </select>
       </div>
     );
   }

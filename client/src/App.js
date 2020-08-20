9a7ac009be050bf41498b6b893c3f0449200073f
JaredMathis <64503422+JaredMathis@users.noreply.github.com>
2020-08-20 11:48:19 -0400

    styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import axios from 'axios';
 import _ from 'lodash';
+import { Form, Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 
 const url = 'http://bible-api.s3-website-us-east-1.amazonaws.com/';
 
@@ -12,13 +14,13 @@ class App extends React.Component {
   }
   componentDidMount = () => {
     axios.get(url + 'translation/kjv/books.json').then(response => {
-      let books = response.data.map(b => { 
+      let books = response.data.map(b => {
         return {
           name: b.replace('-', ' '),
           id: b,
         }
       });
-      this.setState({books});
+      this.setState({ books });
       this.updateBook(books[0].name);
     });
   }
@@ -27,10 +29,10 @@ class App extends React.Component {
     this.updateBook(name);
   }
   updateBook = (name) => {
-    this.setState({selectedBook:name});
+    this.setState({ selectedBook: name });
     axios.get(url + 'translation/kjv/book/' + name + '/chapters.json').then(response => {
       const chapters = response.data;
-      this.setState({chapters});
+      this.setState({ chapters });
       this.updateChapter(chapters[0]);
     });
   }
@@ -39,62 +41,77 @@ class App extends React.Component {
     this.updateChapter(name);
   }
   updateChapter = (c) => {
-    this.setState({selectedChapter:c});
-    axios.get(url + 'translation/kjv/book/' 
-      + this.state.selectedBook + '/chapter/' 
+    this.setState({ selectedChapter: c });
+    axios.get(url + 'translation/kjv/book/'
+      + this.state.selectedBook + '/chapter/'
       + c + '/verses.json').then(response => {
-      const verses = response.data;
-      this.setState({verses});
-      this.updateVerse(verses[0]);
-    });
+        const verses = response.data;
+        this.setState({ verses });
+        this.updateVerse(verses[0]);
+      });
   }
   verseChanged = (e) => {
     let name = e.target.value;
     this.updateVerse(name);
   }
   updateVerse = (v) => {
-    this.setState({selectedVerse:v});
-    this.setState({verse:''});
-    axios.get(url + 'translation/kjv/book/' 
-      + this.state.selectedBook + '/chapter/' 
-      + this.state.selectedChapter + '/verse/' 
+    this.setState({ selectedVerse: v });
+    this.setState({ verse: '' });
+    axios.get(url + 'translation/kjv/book/'
+      + this.state.selectedBook + '/chapter/'
+      + this.state.selectedChapter + '/verse/'
       + v + '.json').then(response => {
-      const verse = response.data.text;
-      console.log({verse});
-      this.setState({verse});
-    });
+        const verse = response.data.text;
+        console.log({ verse });
+        this.setState({ verse });
+      });
   }
   render() {
-    let books = this.state.books 
-      && this.state.books.map(b => 
-      <option key={b.id}>
-        {b.name}
-      </option>);
-    let chapters = this.state.chapters 
-      && this.state.chapters.map(c => 
-      <option key={c}>
-        {c}
-      </option>);
-    let verses = this.state.verses 
-      && this.state.verses.map(v => 
-      <option key={v}>
-        {v}
-      </option>);
+    let books = this.state.books
+      && this.state.books.map(b =>
+        <option key={b.id}>
+          {b.name}
+        </option>);
+    let chapters = this.state.chapters
+      && this.state.chapters.map(c =>
+        <option key={c}>
+          {c}
+        </option>);
+    let verses = this.state.verses
+      && this.state.verses.map(v =>
+        <option key={v}>
+          {v}
+        </option>);
     return (
-      <div>
-        <select onChange={this.bookChanged} value={this.state.selectedBook}>
-          {books}
-        </select>
-        <select onChange={this.chapterChanged} value={this.state.selectedChapter}>
-          {chapters}
-        </select>
-        <select onChange={this.verseChanged} value={this.state.selectedVerse}>
-          {verses}
-        </select>
+      <>
+        <Form.Group>
+          <Form.Label>Book</Form.Label>
+          <Form.Control as="select"
+            onChange={this.bookChanged} 
+            value={this.state.selectedBook}>
+            {books}
+          </Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Chapter</Form.Label>
+          <Form.Control as="select"
+            onChange={this.chapterChanged} 
+            value={this.state.selectedChapter}>
+            {chapters}
+          </Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Verse</Form.Label>
+          <Form.Control as="select"
+            onChange={this.verseChanged} 
+            value={this.state.selectedVerse}>
+            {verses}
+          </Form.Control>
+        </Form.Group>
         <div>
           {this.state.verse}
         </div>
-      </div>
+      </>
     );
   }
 }

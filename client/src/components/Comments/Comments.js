5d2e72e3b8a121145a2afcbc264b8409ee08cbf0
JaredMathis <64503422+JaredMathis@users.noreply.github.com>
2020-08-21 13:10:02 -0400

    styling

--- client/src/components/Comments/Comments.js
+++ client/src/components/Comments/Comments.js
@@ -13,6 +13,7 @@ import {
   ListGroup,
   ListGroupItem,
   Card,
+  InputGroup,
 } from "react-bootstrap";
 
 import {
@@ -44,45 +45,48 @@ class Comments extends React.Component {
             {c.name}: {c.text}
           </ListGroupItem>);
     return (
-      <Card><Card.Body>
-        <FirebaseAuthProvider firebase={this.props.firebase} {...this.props.config}>
-          <SignInOut></SignInOut>
+      <Card className="mt-2">
+        <Card.Body>
+          <FirebaseAuthProvider firebase={this.props.firebase} {...this.props.config}>
+            <SignInOut className="mt-2"></SignInOut>
 
-          <FirestoreProvider firebase={this.props.firebase} {...this.props.config}>
-            <FirebaseAuthConsumer>
-              {({ isSignedIn, firebase }) => {
-                if (isSignedIn === true) {
-                  return (
-                    <>
-                      <FormControl
-                        onChange={(e) =>
-                          this.setState({ commentText: e.target.value })
-                        }
-                        value={this.state.commentText}></FormControl>
-                      <Button
-                        disabled={!this.state.commentText}
-                        onClick={() => {
-                          let { currentUser } = this.props.firebase.auth();
-                          let d = {
-                            text: this.state.commentText,
-                            timestamp: Date.now(),
-                            name: currentUser.displayName,
-                            verseKey: this.props.verseKey,
-                          };
-                          this.props.firebase.firestore().collection("comments").add(d);
-                          this.setState({ commentText: '' })
-                        }}>Comment</Button>
-                    </>
-                  );
-                }
-              }}
-            </FirebaseAuthConsumer>
-            <ListGroup>
-              {comments}
-            </ListGroup>
-          </FirestoreProvider>
-        </FirebaseAuthProvider>
-      </Card.Body></Card>
+            <FirestoreProvider firebase={this.props.firebase} {...this.props.config}>
+              <FirebaseAuthConsumer>
+                {({ isSignedIn, firebase }) => {
+                  if (isSignedIn === true) {
+                    return (
+                      <InputGroup className="mt-2">
+                        <FormControl
+                          onChange={(e) =>
+                            this.setState({ commentText: e.target.value })
+                          }
+                          value={this.state.commentText}></FormControl>
+                        <InputGroup.Append>
+                          <Button
+                            disabled={!this.state.commentText}
+                            onClick={() => {
+                              let { currentUser } = this.props.firebase.auth();
+                              let d = {
+                                text: this.state.commentText,
+                                timestamp: Date.now(),
+                                name: currentUser.displayName,
+                                verseKey: this.props.verseKey,
+                              };
+                              this.props.firebase.firestore().collection("comments").add(d);
+                              this.setState({ commentText: '' })
+                            }}>Comment</Button>
+                        </InputGroup.Append>
+                      </InputGroup>
+                    );
+                  }
+                }}
+              </FirebaseAuthConsumer>
+              <ListGroup className="mt-2">
+                {comments}
+              </ListGroup>
+            </FirestoreProvider>
+          </FirebaseAuthProvider>
+        </Card.Body></Card>
     );
   }
 };
